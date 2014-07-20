0fe052f7610580d4ea4654b6c340b99fd00b9c81
Yanxi <blogbbs@gmail.com>
2014-07-20 10:56:54 +0800

    use javascript instead of coffee

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-require("coffee-script/register");
+var rn = function(max) {
+  return Math.floor((Math.random() * max));
+}
 
-var randomKey = module.exports = require("./lib/generate.coffee");
+var digits = '0123456789'
+var base62 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
+var base30 = '0123456789ABCDFHKLMNPQRSTUVWXYZ'  // 1-9, A-Z exclude(E, G, I, J, O) for human read
+
+exports.generate = function(len, chars) {
+  len = len || 16;
+  chars = chars || base62;
+  var key = '';
+  var charsLen = chars.length;
+  for (var i=0; i<len; i++) {
+    key += chars[rn(charsLen)];
+  }
+  return key;
+};
+
+exports.generateDigits = function(len) {
+  len = len || 16;
+  return exports.generate(len, digits)
+};
+
+exports.generateBase30 = function(len) {
+  len = len || 16;
+  return exports.generate(len, base30)
+}
